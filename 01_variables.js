b0bdcd3214a555c6ac9d8722a3aa74118ee79c19
dilipvanjari <dilipvanjari10@gmail.com>
2022-12-09 22:17:31 +0530

    jslearning
    jslearning

---
+++ 01_variables.js
@@ -0,0 +1,9 @@
+var beforeswap = "========Before swap========="
+console.log(beforeswap)
+var sweety = "sweety"
+console.log(sweety)
+var cutie = "cutie"
+console.log(cutie)
+var afterSwap = "=========afterswap======="
+var temp = sweety
+console.log()
---
+++ hello.js
@@ -0,0 +1,13 @@
+var star;
+console.log(star)
+var hello; //variable declarartion
+hello = "first line of javascript programing" // variable initialization
+console.log(hello);
+var myName = "dilip vanjari"
+console.log(myName)
+var myFullName = "Dilip Ajamal Vanjari"
+console.log(myFullName)
+var myLove = "i love js only"
+console.log(myLove)
+var address = "at post malkheda dist jalgaon"
+console.log(address)
